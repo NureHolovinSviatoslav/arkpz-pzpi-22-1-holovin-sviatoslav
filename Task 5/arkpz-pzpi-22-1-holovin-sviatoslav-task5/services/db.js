1720eceb30e9c759dca0344988f671020051db07
svyatoslav.golovin.2005@gmail.com <svyatoslav.golovin.2005@gmail.com>
2024-12-21 10:56:49 +0200

    fix(main): db_connect

--- Task 5/arkpz-pzpi-22-1-holovin-sviatoslav-task5/services/db.js
+++ Task 5/arkpz-pzpi-22-1-holovin-sviatoslav-task5/services/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.USERNAME,
   process.env.PASSWORD,
   {
-    host: 'localhost',
+    host: process.env.HOST,
     dialect: 'postgres',
     logging: false,
   },
